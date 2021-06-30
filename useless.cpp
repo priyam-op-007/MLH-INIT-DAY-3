7048aa1142a4cce477bf91a16472a6a82c7561e5
Priyaranjan Mishra <priyaranjanmishra000@gmail.com>
2021-06-30 17:03:06 +0530

    Create useless.cpp

---
+++ useless.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<conio.h>
+
+
+
+using namespace std;
+
+
+main()
+
+{
+    char c[100];
+    cout<<"Enter anything ";
+    cin>>c;
+    cout<<"Press any key to continue "<<endl;
+    getch();
+    cout<<"Thank you for wasting your time";
+    getch();
+}
